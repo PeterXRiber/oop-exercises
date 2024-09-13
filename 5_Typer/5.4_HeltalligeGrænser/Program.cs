e62ee1bb2a075b94e9203f43b7ca066f7ec58013
ribz <p.Eisler@hotmail.dk>
2024-09-13 13:43:29 +0200

    Tasks for 5 completed

---
+++ 5_Typer/5.4_HeltalligeGrænser/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("sbyte,byte,short,ushort,int,uint,long,ulong");
+
+byte tooBigByte = 500;
+
+Console.WriteLine(tooBigByte);
---
+++ 5_Typer/5.5_Casting/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int i = 20;
+long l = 205;
+
+i = (int)l;
+
+Console.WriteLine(l);
+
+float f = 20.5120f;
+double d = 50.51206996;
+
+f = (float)d;
+
+Console.WriteLine(f);
