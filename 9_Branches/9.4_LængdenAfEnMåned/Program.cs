26422695430a0b9d10c672f24956856bcff25c3e
ribz <p.Eisler@hotmail.dk>
2024-09-24 10:13:51 +0200

    Task 9.4 complete

---
+++ 9_Branches/9.4_LængdenAfEnMåned/Program.cs
@@ -0,0 +1,76 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int jan = 1;
+int feb = 2;
+int mar = 3;
+int apr = 4;
+int may = 5;
+int jun = 6;
+int jul = 7;
+int aug = 8;
+int sep = 9;
+int oct = 10;
+int nov = 11;
+int dec = 12;
+
+int daysInJan = 0;
+int daysInFeb = 0;
+int daysInMar = 0;
+int daysInApr = 31;
+int daysInMay = 30;
+int daysInJun = 31;
+int daysInJul = 30;
+int daysInAug = 31;
+int daysInSep = 30;
+int daysInOct = 31;
+int daysInNov = 30;
+int daysInDec = 31;
+
+if (jan == 1) {
+	daysInJan = 31;
+	Console.WriteLine(daysInJan);
+} 
+if (feb == 2) {
+	daysInFeb = 28;
+	Console.WriteLine(daysInFeb);
+}
+if (mar == 3) {
+	daysInMar = 30;
+	Console.WriteLine(daysInMar);
+}
+if (apr == 4) {
+	daysInApr = 31;
+	Console.WriteLine(daysInApr);
+}
+if (may == 5) {
+	daysInMay = 30;
+	Console.WriteLine(daysInMay);
+}
+if (jun == 6) {
+	daysInJun = 31;
+	Console.WriteLine(daysInJun);
+}
+if (jul == 7) {
+	daysInJul = 30;
+	Console.WriteLine(daysInJul);
+}
+if (aug == 8) {
+	daysInAug = 31;
+	Console.WriteLine(daysInAug);
+}
+if (sep == 9) {
+	daysInSep = 30;
+	Console.WriteLine(daysInSep);
+}
+if (oct == 10) {
+	daysInOct = 31;
+	Console.WriteLine(daysInOct);
+}
+if (nov == 11) {
+	daysInNov = 30;
+	Console.WriteLine(daysInNov);
+}
+if (dec == 12) {
+	daysInDec = 31;
+	Console.WriteLine(daysInDec);
+}
