0f4adc3cdf442f0c2868704a5fe6995ddb2e0c47
ribz <p.Eisler@hotmail.dk>
2024-09-25 12:46:16 +0200

    15.1 complete

---
+++ 15_Exceptions/15.1_Indeksering/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+int iterationer = 10;
+int[] array = {1, 2, 3, 4, 5};
+// increment
+
+for (int i = 0 ; i < iterationer ; i++)
+{
+	try{
+		array[i]++;
+	} catch (IndexOutOfRangeException e) {
+		break;
+	}
+}
+
+// print
+for (int i = 0 ; i < array.Length ; i++)
+{
+Console.WriteLine(array[i]);
+}
+
+
+
+// 5. Er dette den korrekte løsning på problemet, og hvorfor?
+// 
