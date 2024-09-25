8659a6d035fbf384202818b17cfb9cbd11d4c983
ribz <p.Eisler@hotmail.dk>
2024-09-25 13:12:03 +0200

    15.2 complete

---
+++ 15_Exceptions/15.2_Kontoer/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int[] accounts = { 903, 716, 67 };
+int GetAccountNumber(){
+Console.WriteLine("Enter an account number: ");
+
+return Convert.ToInt32(Console.ReadLine());
+
+}
+void PrintAccountState(int accountId){
+Console.WriteLine("Account " + accountId + " contains " + accounts[accountId]);
+}
+
+while (true)
+{
+	int accountId = GetAccountNumber();
+	
+	try{
+		PrintAccountState(accountId);
+	} catch (IndexOutOfRangeException e) {
+		Console.WriteLine("That shit you typed was whack AF");
+		break;
+	}
+
+
+}
