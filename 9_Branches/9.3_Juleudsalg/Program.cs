f5d0863dd492af6025e9b7075a9d856078291709
ribz <p.Eisler@hotmail.dk>
2024-09-26 09:22:22 +0200

    Section 9 complete

---
+++ 9_Branches/9.3_Juleudsalg/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+long secSinceNewyear = 21816000;
+long secSinceNewyearInDays = secSinceNewyear / 86400;
+double priceInDK = 599.95;
+
+double priceDiscounted = 599.95 * 0.70;
+// 334 = days until 1. of december
+if (secSinceNewyearInDays < 334) {
+	Console.WriteLine("Womp womp no discount yet. The price is still: " + priceInDK);
+} else if (secSinceNewyearInDays > 334) {
+	Console.WriteLine("The product is discounted with 30%. The current price is now: " + (priceDiscounted));
+}
+
+Console.WriteLine(secSinceNewyearInDays);
+Console.WriteLine(priceDiscounted);
---
+++ 9_Branches/9.5_Ferie/Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+for (int i = 0; i < 1; i++) {
+	
+int jan = 1;
+int feb = 2;
+int mar = 3;
+int apr = 4;
+int may = 5;
+int jun = 6;
+int jul = 7;
+int aug = 8;
+int sep = 9;
+int oct = 10;
+int nov = 11;
+int dec = 12;
+
+if (jan == 1){
+	Console.WriteLine("Hard Work");
+}
+if (feb == 2){
+	Console.WriteLine("Hard Work");
+}
+if (mar == 3){
+	Console.WriteLine("Hard Work");
+}
+if (apr == 4) {
+Console.WriteLine("Vaca-time! It is easter");
+ } 
+if (may == 5){
+	Console.WriteLine("Hard Work");
+}
+if (jun == 6){
+	Console.WriteLine("Hard Work");
+}
+if (jul == 7) {
+	Console.WriteLine("Vaca-time! It is summer");
+ }
+if (aug == 8) {
+	Console.WriteLine("Vaca-time! It is (still) summer");
+ }
+if (sep == 9){
+	Console.WriteLine("Hard Work");
+}
+if (oct == 10) {
+	Console.WriteLine("Vaca-time! It is autumn-break");
+ }
+if (nov == 11){
+	Console.WriteLine("Hard Work");
+}
+if (dec == 12) {
+	Console.WriteLine("Vaca-time! It is christmas-break");
+ }
+
+}
+
+
+
